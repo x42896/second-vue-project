1434939d1bd749eda10495e558adefbc6cb94017
x42896 <x42896@gmail.com>
2025-08-10 22:46:01 +0300

    Первичное создание компонентов

---
+++ src/components/User.vue
@@ -0,0 +1,17 @@
+<template>
+    <!-- Данные не будем тут перебирать по этому цикл не нужен -->
+    <div class="user">
+        <h3>{{ el.name }}</h3>
+        <p>{{ el.name }} – <b>{{ el.pass }}</b></p>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
